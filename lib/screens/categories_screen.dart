ba984cd4939b2c296f83cb5c7782e9fb746035db
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-02-28 18:58:02 +0100

    switched to bottom bar to switch to favorites instead of topbar for a better ux/ui

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -7,19 +7,21 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
-      padding: EdgeInsets.all(25),
+      padding: const EdgeInsets.all(25),
       children: DUMMY_CATEGORIES
-          .map((catData) => CategoryItem(
-                catData.id,
-                catData.title,
-                catData.color,
-              ))
+          .map(
+            (catData) => CategoryItem(
+              catData.id,
+              catData.title,
+              catData.color,
+            ),
+          )
           .toList(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200,
         childAspectRatio: 3 / 2,
-        mainAxisSpacing: 20,
         crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
     );
   }
